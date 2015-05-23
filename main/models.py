108402a9b5ede62461e921d2c869b53e453c6bf2
Orest Voloschuk <orest.v@gmail.com>
2015-05-23 18:14:00 +0300

    Initial DB.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, null=False)
+    text = models.CharField(max_length=2**10 * 50, null=False)
+    parent = models.OneToOneField(to='Comment', default=None)
